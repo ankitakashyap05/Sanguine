e6e25d2c0c8e2d951f9185429cfa6d725fc3af75
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2022-02-19 18:06:40 +0530

    add report and userinfo integration model

--- sasa/models.py
+++ sasa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class UserCompleteInfo(models.Model):
+    username=models.CharField(max_length=100)
+    email=models.EmailField()
+    password=models.CharField(max_length=200)
+    cpassword=models.CharField(max_length=200)
+    age=models.PositiveIntegerField()
+    occupation=models.CharField(max_length=200)
+    mobile=models.CharField(max_length=10)
+    gender=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username[:50]
+
+class Report(models.Model):
+    username=models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+    date=models.DateField()
+    time=models.TimeField()
+    about=models.CharField(max_length=1000)
+    police_report=models.CharField(max_length=100)
+    link=models.CharField(max_length=300)
+    dor=models.DateTimeField(default=datetime.now)
+    help=models.CharField(max_length=150)
