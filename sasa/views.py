347695af15db99c8b615059b4eeb900366d83a95
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2022-02-19 14:58:53 +0530

    add home page

--- sasa/views.py
+++ sasa/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from sanguine import settings
+from django.shortcuts import redirect,render
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
+def homepage(request):
+    return render(request,"index.html")
