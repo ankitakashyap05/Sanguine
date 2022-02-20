10cdc04eaf9fa606b1c04eb57866976705b46f8f
ACosmicWolf <cosmicwolf009@gmail.com>
2022-02-20 18:16:58 +0530

    sixthg try

--- sanguine/settings.py
+++ sanguine/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fbk#@dj=fwim8)v&_f^esa*+8b%#p5b90h-h56_rf3xjs_q+db
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sanguinesasa.herokuapp.com']
-
+CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com','https://*.127.0.0.1']
 #CSRF_COOKIE_DOMAIN = 'sanguinesasa.herokuapp.com'
 
 # Application definition
