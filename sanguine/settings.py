4ed783a2727caac70c38745d32313f1aba4c9a77
ACosmicWolf <cosmicwolf009@gmail.com>
2022-02-20 17:58:21 +0530

    fifth try

--- sanguine/settings.py
+++ sanguine/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fbk#@dj=fwim8)v&_f^esa*+8b%#p5b90h-h56_rf3xjs_q+db'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sanguinesasa.herokuapp.com']
 
